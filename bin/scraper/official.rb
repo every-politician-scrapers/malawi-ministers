7110f1ff1d3c2f6cf2b59acee208e4daf4e7af2e
Tony Bowden <tony@tmtm.com>
2021-11-25 11:29:03 +0200

    initial scraper

--- bin/scraper/official.rb
+++ bin/scraper/official.rb
@@ -6,18 +6,41 @@ require 'pry'
 
 class MemberList
   class Member
+    # Most members have separate divs,
+    # but some have "Name: Positition" in the name field
+
     def name
-      noko.css('.name').text.tidy
+      return raw_name.split(':').first.tidy if raw_position.empty?
+
+      raw_name
     end
 
     def position
-      noko.css('.position').text.tidy
+      derived_position.split(/ and (?=Minister)/).flat_map do |posn|
+        posn.split(/ (?=Commander)/)
+      end
+    end
+
+    private
+
+    def raw_name
+      noko.css('.sppb-person-designation').text.sub('Honourable', '').tidy
+    end
+
+    def raw_position
+      noko.css('.sppb-person-introtext').text.tidy
+    end
+
+    def derived_position
+      return raw_name.split(':').last.tidy if raw_position.empty?
+
+      raw_position
     end
   end
 
   class Members
     def member_container
-      noko.css('.member')
+      noko.css('.sppb-person-information-wrap')
     end
   end
 end
